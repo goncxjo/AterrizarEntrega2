5318115052bd59f1ad69e59b6ab49d0eeed1774d
gonza5569 <gonzalo.moreira@live.com.ar>
2019-04-27 22:03:51 -0300

    se agrega el comunicador como parametro. se implementa la busqueda de vuelos iterando por aerolineas

--- src/com/aterrizar/modelo/Comunicador.java
+++ src/com/aterrizar/modelo/Comunicador.java
@@ -1,12 +1,73 @@
 package com.aterrizar.modelo;
 
-import com.aterrizar.modelo.Aerolinea.IAerolineaLanchita;
+import com.aterrizar.modelo.Aerolinea.*;
+import com.aterrizar.modelo.Usuario.Usuario;
+import com.aterrizar.modelo.Vuelo.FiltroVuelo;
 import com.aterrizar.modelo.Vuelo.Vuelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * El comunicador nos proporciona herramientas
+ * para comunicarnos entre aterrizar.com y las aerolineas.
+ * */
 public class Comunicador {
-    List<Vuelo> buscarVuelos(IAerolineaLanchita aerolineaLanchita, Vuelo vuelo) {
-        return null;
+    private Aerolinea[] aerolineas;
+
+    public Comunicador(Aerolinea[] aerolineas) {
+        this.aerolineas = aerolineas;
     }
+
+    /**
+     * Este método itera por todas las aerolineas en aterrizar.com
+     * en busca de vuelos disponibles
+     * @param filtroVuelo los filtros proporcionados por el usuario
+     * @param usuario el usuario que realizó la consulta
+     * */
+    public List<Vuelo> buscarVuelos(FiltroVuelo filtroVuelo, Usuario usuario) {
+        List<Vuelo> vuelos = new ArrayList<>();
+
+        for (Aerolinea item : aerolineas) {
+
+            if(item instanceof AerolineaLanchitaImplementacion) { // Si la aerolinea es una instancia de "Lanchita", entonces buscar vuelos en "Lanchita"...
+                vuelos = this.buscarVuelos(
+                        (AerolineaLanchita)item // importante castear para que lo reconozca el método
+                        , filtroVuelo
+                        , usuario
+                );
+            } else if(item instanceof AerolineaFakeImplementacion) { // Si la aerolinea es una instancia de "Fake", entonces buscar vuelos en "Fake"...
+                vuelos = this.buscarVuelos(
+                        (AerolineaFakeImplementacion)item
+                        , filtroVuelo
+                        , usuario
+                );
+            }
+        }
+
+        return vuelos;
+    }
+
+    // TODO: implementar método
+    /**
+     * Busca los vuelos en Aerolineas Lanchita
+     * @param aerolineaLanchita con esta instancia, puedo acceder a los métodos proporcionados por la aerolinea
+     * @param filtroVuelo los filtros proporcionados por el usuario
+     * @param usuario el usuario que realizó la consulta
+     * */
+    public List<Vuelo> buscarVuelos(AerolineaLanchita aerolineaLanchita, FiltroVuelo filtroVuelo, Usuario usuario) {
+        List<Vuelo> vuelos = new ArrayList<>();
+        return vuelos;
+    }
+
+    /**
+     * Busca los vuelos en Aerolineas Fake (sólo para pruebas)
+     * */
+    public List<Vuelo> buscarVuelos(AerolineaFake aerolineaFake, FiltroVuelo filtroVuelo, Usuario usuario) {
+        List<Vuelo> vuelos = new ArrayList<>();
+        return vuelos;
+    }
+
+
+
 }
--- src/com/aterrizar/modelo/Sistema.java
+++ src/com/aterrizar/modelo/Sistema.java
@@ -1,17 +1,24 @@
 package com.aterrizar.modelo;
 
 import com.aterrizar.modelo.Asiento.Asiento;
+import com.aterrizar.modelo.Ubicacion.Ubicacion;
 import com.aterrizar.modelo.Usuario.Usuario;
+import com.aterrizar.modelo.Vuelo.FiltroVuelo;
 import com.aterrizar.modelo.Vuelo.Vuelo;
 
 import java.util.List;
 
 public class Sistema {
-    public void registrarUsuario(String nombre, String apellido, String DNI) {}
+    Comunicador comunicador;
 
-    public List<Vuelo> buscar(Vuelo vuelo, Usuario usuario) {
+    // TODO: pendiente implementacion
+    public void registrarUsuario(String nombre, String apellido, int DNI) {}
+
+    // TODO: pendiente implementacion
+    public List<Vuelo> buscarVuelos(FiltroVuelo filtroVuelo, Usuario usuario) {
         return null;
     }
 
+    // TODO: pendiente implementacion
     public void comprar(Vuelo vuelo, Asiento asiento, Usuario usuario) {}
 }
